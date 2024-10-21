a1a58f1b8c22d33fe48cdff0e4d8dde793b073fe
rakibul-hasan678 <fasterhasan0@gmail.com>
2024-10-21 09:55:49 +0600

    f

---
+++ asset/js/custom.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+//   $('#toggleButton').click(function () {
+//     $(this).toggleClass('bg-blue-500 bg-green-500');
+//   });
+// });
+
+// $(document).ready(function () {
+//   $('#mobileMenuButton').click(function () {
+//     $('.mobile-menu').toggleClass('hidden');
+//   });
+// });
+$(document).ready(function () {
+  $('#menu-btn').click(function () {
+    $('#mobile-menu').toggleClass('hidden');
+  });
+});
